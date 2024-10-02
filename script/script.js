a778553e8cc6410ca443f66538269e4ee68a0470
Jay Christy <jaychristy0523@gmail.com>
2024-10-02 21:47:34 +0700

    update detail.html

--- script/script.js
+++ script/script.js
@@ -4,7 +4,24 @@ fetch('data/bamboo.json')
 .then(data => {
     bamboo = data;
     console.log(bamboo);
+    addDataToHTML();
 })
+
+let listProduct =  document.querySelector('.listProduct');
+
+function addDataToHTML() {
+
+    bamboo.forEach(bamboo => {
+        let newProduct = document.createElement('a');
+        newProduct.href = '/detail.html?id=' + bamboo.id;
+        newProduct.classList.add('item');
+        newProduct.innerHTML = `
+        <img src="${bamboo.image}" alt="">
+        <h2>${bamboo.name}</h2>`;
+
+        listProduct.appendChild(newProduct);
+    })
+}
 
 let pandan = null;
 fetch('data/pandan.json')
