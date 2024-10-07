dd316d65a0e3081c4a59cfb9cda5a439b1525a62
Jay Christy <jaychristy0523@gmail.com>
2024-10-07 12:29:51 +0700

    detail update

--- tester.js
+++ tester.js
@@ -1,35 +1,40 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+// let next = document.querySelector('.next')
+// let prev = document.querySelector('.prev')
 
-next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
+// next.addEventListener('click', function(){
+//     let items = document.querySelectorAll('.item')
+//     document.querySelector('.slide').appendChild(items[0])
+// })
 
-prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
-})
+// prev.addEventListener('click', function(){
+//     let items = document.querySelectorAll('.item')
+//     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+// })
 
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
   
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
+
+
+const myDiv = document.getElementById("div-test");
+
+myDiv.innerHTML = '<h1> HALO </h1>';
