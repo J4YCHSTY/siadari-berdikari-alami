a14f0fb2a5e32af121bf70906c815a550bb5039d
Jay Christy <jaychristy0523@gmail.com>
2024-10-09 02:26:10 +0700

    add link
    add link for navigation page

--- script/script.js
+++ script/script.js
@@ -1,43 +1,43 @@
-let bamboo = null;
-fetch('data/bamboo.json')
-.then(response => response.json())
-.then(data => {
-    bamboo = data;
-    console.log(bamboo);
-    addDataToHTML();
-})
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get('category');
 
-let listProduct =  document.querySelector('.listProduct');
-
-function addDataToHTML() {
-
-    bamboo.forEach(bamboo => {
-        let newProduct = document.createElement('div');
-        newProduct.classList.add('col-lg-4', 'col-md-12', 'mb-4');
-        newProduct.href = '/detail.html?id=' + bamboo.id;
-        newProduct.classList.add('item');
-        newProduct.innerHTML = `
-        <div class="bg-image hover-zoom ripple shadow-1-strong rounded listProduct">
-            <img src="${bamboo.image}" class="w-100" />
-            <a href="#">
-                <div class="card-title">
-                <div class="d-flex justify-content-center align-items-center h-100">
-                    <h5><span class="badge bg-body-tertiary pt-2 mt-3 text-light">${bamboo.name}</span></h5>
-                </div>
-                </div>
-            </a>
-        </div>`;
-
-        listProduct.appendChild(newProduct);
-    })
-
-    
+function fetchProductData(category) {
+    let jsonFile = `data/${category}.json`;  
+    fetch(jsonFile)
+        .then(response => response.json())
+        .then(data => {
+            displayProductList(data);
+        })
+        .catch(error => console.error('Error fetching product data:', error));
 }
 
-let pandan = null;
-fetch('data/pandan.json')
-.then(response => response.json())
-.then(data => {
-    pandan = data;
-    console.log(pandan);
-})
+
+function displayProductList(products) {
+    let listProduct = document.querySelector('.listProduct');
+    listProduct.innerHTML = '';  
+
+    products.forEach(product => {
+        let productCard = `
+            <div class="col-lg-4 col-md-12 mb-4">
+                <div class="bg-image hover-zoom ripple shadow-1-strong rounded listProduct">
+                    <img src="${product.image}" class="w-100" />
+                    <a href="#">
+                        <div class="card-title">
+                            <div class="d-flex justify-content-center align-items-center h-100">
+                                <h5><span class="badge bg-body-tertiary pt-2 mt-3 text-light">${product.name}</span></h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        `;
+        listProduct.innerHTML += productCard;
+    });
+}
+
+
+if (category) {
+    fetchProductData(category);
+} else {
+    console.error('Category not found in URL');
+}
